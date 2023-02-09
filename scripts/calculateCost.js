8606cd724da2e77819bdd8282eb8e1d916dc6818
Abram31 <a447623993@gmail.com>
2023-02-09 20:28:22 +0100

    feat: add diferent costs for standard

--- scripts/calculateCost.js
+++ scripts/calculateCost.js
@@ -1,16 +1,22 @@
 // Стоимость перевозки
-// const costs = {
-//     standard: 22,
-//     comfort: 30,
-//     business: 40,
-//     bus: 40
-// }
+const costs = {
+    standard: [25,22,20],
+    comfort: 30,
+    business: 40,
+    bus: 40
+}
 
 export const calculateCost = (distance, carClass) => {
     if (distance.includes('км')) {
         console.log(distance, carClass);
         const number = distance.slice(0, -3).split(',').join('.').split('').filter((item) => item.trim() !== '').join('');
-        const result = (costs[`${carClass}`] * Number(number).toFixed(0));
+        let result;
+        if (carClass === 'standard') {
+            const cost = Number(number) <= 200 ? costs.standard[0] : Number(number) <= 1000 ? costs.standard[1] : costs.standard[2]
+            result = (cost * Number(number).toFixed(0));   
+        } else {
+            result = (costs[`${carClass}`] * Number(number).toFixed(0));   
+        }
         return result;
     } return costs[`${carClass}`];
 }
