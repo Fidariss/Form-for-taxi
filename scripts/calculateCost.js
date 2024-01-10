2bdd3d88ddb00e4f6fe657f1cfc0148759d86194
Fidariss <123474887+Fidariss@users.noreply.github.com>
2024-01-10 21:40:36 +0400

    Update calculateCost.js

--- scripts/calculateCost.js
+++ scripts/calculateCost.js
@@ -7,17 +7,22 @@
 // }
 
 export const calculateCost = (distance, carClass) => {
+
     if (distance.includes('км')) {
-        console.log(distance, carClass);
+        
         const number = distance.slice(0, -3).split(',').join('.').split('').filter((item) => item.trim() !== '').join('');
         let result;
         if (carClass === 'standard') {
             const cost = Number(number) <= 200 ? costs.standard[0] : Number(number) <= 1000 ? costs.standard[1] : costs.standard[2]
             result = (cost * Number(number).toFixed(0));   
-        } else {
+        }
+        else {
+
             result = (costs[`${carClass}`] * Number(number).toFixed(0));   
         }
-        return result;
-    } return costs[`${carClass}`];
-}
 
+        return result;
+    }
+    
+    return costs[`${carClass}`];
+}
