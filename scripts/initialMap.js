34c0a5fb0b9f77bca50a796ab82c1ce5cbb7aa55
Fidariss <123474887+Fidariss@users.noreply.github.com>
2024-01-10 21:41:26 +0400

    Update initialMap.js

--- scripts/initialMap.js
+++ scripts/initialMap.js
@@ -1,8 +1,8 @@
 export function init() {
     // Создаем выпадающую панель с поисковыми подсказками и прикрепляем ее к HTML-элементу по его id.
-    var suggestView1 = new ymaps.SuggestView('departure-city');
-    var suggestView2 = new ymaps.SuggestView('arrival-city');
-document.addEventListener('error',(err)=> console.log(err))
+    var suggestView1 = new ymaps.suggest('departure-city');
+    var suggestView2 = new ymaps.suggest('arrival-city');
+document.addEventListener('error',(err)=> console.error(err))
     // Задаем собственный провайдер поисковых подсказок и максимальное количество результатов.
     // var suggestView2 = new ymaps.SuggestView('suggest2', { provider: provider, results: 3 });
 }
